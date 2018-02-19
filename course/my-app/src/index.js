77fb7365cdf6a3db9f2b3763f58398c1e03690db
Alex Gonzalez <alexgonrod83@gmail.com>
2018-02-19 09:14:32 +0100

    Reorgonize

--- course/my-app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './components/App.css';
-import TaskApp from './components/TaskApp';
-
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<TaskApp />, document.getElementById('root'));
-registerServiceWorker();
