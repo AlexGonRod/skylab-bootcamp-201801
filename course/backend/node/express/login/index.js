dd43a58f1f6a26fc5e3239f110e9d1a0b785f9fb
Alex Gonzalez <alexgonrod83@gmail.com>
2018-02-23 09:20:52 +0100

    Fixed Files

---
+++ course/backend/node/express/login/index.js
@@ -0,0 +1,28 @@
+require ('dotenv').config()
+
+const bodyParser = require('body-parser')
+const express = require('express')
+
+const app = express()
+
+let users = {}
+
+const urlencoderParser = bodyParser.urlencoded({extended: false})
+
+app.post('/api/users', urlencoderParser, (req, res) => {
+
+    const user = {
+        username: req.body.username,
+        password: req.body.password
+
+        
+    }
+
+    res.send(`You are logged`)
+})
+
+const port = process.env.PORT
+app.listen(port, () => console.log(`Server running in port ${port}`))
+
+
+
