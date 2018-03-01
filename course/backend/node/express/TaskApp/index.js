2991e1b88ac75206c4a869fc35f9741dc81da94d
Alex Gonzalez <alexgonrod83@gmail.com>
2018-03-01 09:51:58 +0100

    Adding Task-Api

--- course/backend/node/express/TaskApp/index.js
+++ course/backend/node/express/TaskApp/index.js
@@ -1,38 +1,77 @@
-require ('dotenv').config()
-
 const express = require('express')
-const bodyParser = require('body-parser')
-
+require('dotenv').config()
 
 const app = express()
-
-const tasks = []
-let ids = 0
-
-
-app.get('/api/tasks', (req, res) => res.json(ok('Task list succeded', tasks.map(({text}) => ({text})))))
+const bodyParser = require('body-parser')
 
 const jsonBodyParser = bodyParser.json()
-app.post('/api/tasks', jsonBodyParser,  (req, res) => {
-    const { id, text } = req.body
 
-    tasks.push({ text })
-    ids ++
-    res.json('OK, Task registration succedeed')
+let tasks = []
+let ids = 1
+
+app.post('/api/tasks', jsonBodyParser, (req, res) => {
+    const { text } = req.body
+    if(!text < 0) return res.json('Invalid task registration')
+    if(tasks.length !== 0){ tasks[tasks.length -1].id = ids++ }
+    tasks.push({id: ids, text, done: false})
+    res.json('PUT ok')
 })
 
-function ok(message, data) {
-    const res = { status: 'OK', message}
-    if (data) res.data = data
-     
-    return res
-}
+app.get('/api/tasks', (req, res) => {
+     const allTasks = tasks.map(task => {
+         return task.text
+     })
+    res.json(allTasks)
+})
 
+app.get('/api/tasks/done', (req, res) => {
+   
+   const tasksDone = tasks.filter(task => {
+       return task.done === true
+   })
+   
+    res.json(tasksDone.map(({text})=>({text})))
+})
 
+app.get('/api/tasks/todo', (req, res) => {
+    const todo = tasks.filter(task => {
+        return task.done === false
+    })
+    res.json(todo.map(({text}) => ({text})))
+})
 
+app.put('/api/tasks/:id', (req, res) => {
+   const {id } = req.params
+   const find = tasks.find(task => {
+      return task.id === parseInt(id)
+   })
+   find.done = true
+    res.send('OK')
+})
 
+app.delete('/api/tasks/:id', (req, res) => {
+   const { id } = req.params
+   const find = tasks.findIndex(task => {
+       return task.id = parseInt(id)
+   })
+   tasks.splice(find, 1)
+    res.send('Deletion OK')
+})
 
-// SRVER listener
-const port = process.env.PORT
+app.delete('/api/tasks', (req, res)=> {
+    tasks = []
+    res.send('All deleted')
+})
 
-app.listen(port, () => console.log(`Port listening on port ${port}`))
+app.patch('/api/tasks/:id', jsonBodyParser, (req, res) => {
+    const { id } = req.params
+    const { text } = req.body
+    const find = tasks.find(task => {
+        return task.text = text
+    })
+    res.json(find)
+})
+
+const port = process.env.PORT 
+
+app.listen(port, () => console.log(`Server listening on port ${port}`))
